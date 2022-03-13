0e137a49e81d37c9fb9053467e441c76655a92c7
AlvinLin <alvinvalensia24@gmail.com>
2022-03-13 01:43:43 -0600

    menambahkan component button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from "react";
+import propTypes from "prop-types";
+import Button from "./Button";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button text="Add" color="green" />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: propTypes.string.isRequired,
 };
 
 export default Header;
